af1e7474c66cb5307c74c22e9929bfb076eca0c8
popo <miss.poepoe23@gmail.com>
2023-08-03 18:39:25 +0100

    Projects: hide/show header

--- src/app/ui/components/Project/Project.js
+++ src/app/ui/components/Project/Project.js
@@ -8,6 +8,7 @@ import ProjectItem from './ProjectItem';
 
 class Project {
 	domProject;
+	domHeader;
 	listItems = [];
 	state = {
 		isOpen: false,
@@ -19,7 +20,7 @@ class Project {
 		this.listItems = items.map((item) => new ProjectItem(item));
 		this.closeBtnDOM = document.querySelector('.project-close-btn');
 		this.domProject = document.getElementById('project');
-
+		this.domHeader = document.querySelector('header');
 		this.init();
 	}
 
@@ -35,11 +36,24 @@ class Project {
 		console.log('show project');
 
 		this.domProject.style.display = 'block';
-		this.tlFadeIn = gsap.timeline().to(this.domProject, {
-			autoAlpha: 1,
-			ease: 'Power2.out',
-			duration: 1,
-		});
+		this.domHeader.style.pointerEvents = 'none';
+
+		this.tlFadeIn = gsap
+			.timeline()
+			.to(this.domProject, {
+				autoAlpha: 1,
+				ease: 'Power2.out',
+				duration: 0.6,
+			})
+			.to(
+				this.domHeader,
+				{
+					opacity: 0,
+					duration: 0.3,
+					ease: 'Power2.out',
+				},
+				0,
+			);
 
 		this.tlFadeIn.play();
 	}
@@ -47,14 +61,28 @@ class Project {
 	hide() {
 		console.log('hide project');
 
-		this.tlFadeOut = gsap.timeline().to(this.domProject, {
-			autoAlpha: 0,
-			ease: 'Power2.out',
-			duration: 1,
-			onComplete: () => {
-				this.domProject.style.display = 'none';
-			},
-		});
+		this.tlFadeOut = gsap
+			.timeline()
+			.to(this.domProject, {
+				autoAlpha: 0,
+				ease: 'Power2.out',
+				duration: 0.8,
+				onComplete: () => {
+					this.domProject.style.display = 'none';
+				},
+			})
+			.to(
+				this.domHeader,
+				{
+					opacity: 1,
+					duration: 0.3,
+					ease: 'Power2.out',
+					onComplete: () => {
+						this.domHeader.style.pointerEvents = 'auto';
+					},
+				},
+				0,
+			);
 
 		this.tlFadeOut.play();
 	}
