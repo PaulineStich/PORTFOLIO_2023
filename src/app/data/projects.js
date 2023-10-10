4d08dba27c097f37749646c3a95a595897a56990
popo <miss.poepoe23@gmail.com>
2023-10-10 13:18:48 +0100

    Assets: png LOW and HIGH, webp

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -1,31 +1,20 @@
-import testImage from '../../../public/assets/images/test.png';
-import dinosaurImage from '../../../public/assets/images/dinosaur.png';
-import plantsImage from '../../../public/assets/images/plants.png';
-import unchainedMelodyImage from '../../../public/assets/images/unchaInedMelody.png';
-
 import myLittleStorybookImage1 from '../../../public/assets/images/my_little_storybook/image10.webp';
 import myLittleStorybookImage2 from '../../../public/assets/images/my_little_storybook/image9.webp';
 import myLittleStorybookImage3 from '../../../public/assets/images/my_little_storybook/image11.webp';
 
-import chooChooWorldImage1 from '../../../public/assets/images/choo_choo/choochoo1.png';
-import chooChooWorldImage2 from '../../../public/assets/images/choo_choo/choochoo3.png';
-import chooChooWorldImage3 from '../../../public/assets/images/choo_choo/clay.png';
+import chooChooWorldImage1 from '../../../public/assets/images/choo_choo/LOW/choochoo1.png';
+import chooChooWorldImage2 from '../../../public/assets/images/choo_choo/choochoo3.webp';
+import chooChooWorldImage3 from '../../../public/assets/images/choo_choo/clay.webp';
 
-import feuxDeForestImage1 from '../../../public/assets/images/feux_de_forest/screencapture-fireforest.png';
+import feuxDeForestImage1 from '../../../public/assets/images/feux_de_forest/screencapture-fireforest.webp';
 
-import djeliImage1 from '../../../public/assets/images/djeli/image1.png';
-import djeliImage2 from '../../../public/assets/images/djeli/image2.png';
-import djeliImage3 from '../../../public/assets/images/djeli/image3.png';
+import djeliImage1 from '../../../public/assets/images/djeli/image1.webp';
+import djeliImage2 from '../../../public/assets/images/djeli/image2.webp';
+import djeliImage3 from '../../../public/assets/images/djeli/image3.webp';
 
-import originesImage1 from '../../../public/assets/images/origines/origines1.png';
-import originesImage2 from '../../../public/assets/images/origines/origines2.png';
-import originesImage3 from '../../../public/assets/images/origines/origines3.png';
-import originesImage4 from '../../../public/assets/images/origines/origines4.png';
-import originesImage5 from '../../../public/assets/images/origines/origines5.png';
-import originesImage6 from '../../../public/assets/images/origines/origines6.png';
-import originesImage7 from '../../../public/assets/images/origines/origines7.png';
-import originesImage8 from '../../../public/assets/images/origines/origines8.png';
-import originesImage9 from '../../../public/assets/images/origines/origines9.png';
+import originesImage1 from '../../../public/assets/images/origines/LOW/origines6.png';
+import originesImage2 from '../../../public/assets/images/origines/origines2.webp';
+import originesImage3 from '../../../public/assets/images/origines/origines3.webp';
 
 const data = [
 	// {
