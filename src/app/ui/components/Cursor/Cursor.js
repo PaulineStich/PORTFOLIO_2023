4976ef1ca2ae45c09af60d744ba8b98624215666
popo <miss.poepoe23@gmail.com>
2023-10-10 14:13:25 +0100

    Cursor: hide it for now

--- src/app/ui/components/Cursor/Cursor.js
+++ src/app/ui/components/Cursor/Cursor.js
@@ -3,124 +3,121 @@ import math from '@app/utils/math';
 import properties from '@core/properties';
 import { HOVER_STATE } from '../../constants';
 
-class Cursor {
-	constructor() {
-		this.cursor = document.querySelector('.cursor');
-		this.cursorSphere = document.querySelector('.cursor_sphere');
-		this.cursorContent = document.querySelector('.cursor_container');
-		this.cursorContentText = this.cursorContent.querySelector('p');
+// class Cursor {
+// 	constructor() {
+// 		// this.cursor = document.querySelector('.cursor');
+// 		// this.cursorSphere = document.querySelector('.cursor_sphere');
+// 		// this.cursorContent = document.querySelector('.cursor_container');
+// 		// this.cursorContentText = this.cursorContent.querySelector('p');
 
-		this.active = false;
-		this.allowAnimation = false;
+// 		// this.active = false;
+// 		// this.allowAnimation = false;
 
-		this.state = {
-			position: {
-				cursor: {},
-			}, // position of dom cursor
-			x: { previous: 0, current: 0 }, // position of mouse
-			y: { previous: 0, current: 0 }, // position of mouse
-			scale: { previous: 1, current: 1 },
-			opacityCursor: { previous: 0, current: 0.4 },
-			opacityText: { previous: 0, current: 0 },
-			angle: { previous: 0, current: 0 },
-			squeeze: { previous: 0, current: 0 },
-			force: 30,
-		};
-	}
+// 		// this.state = {
+// 		// 	position: {
+// 		// 		cursor: {},
+// 		// 	}, // position of dom cursor
+// 		// 	x: { previous: 0, current: 0 }, // position of mouse
+// 		// 	y: { previous: 0, current: 0 }, // position of mouse
+// 		// 	scale: { previous: 1, current: 1 },
+// 		// 	opacityCursor: { previous: 0, current: 0.4 },
+// 		// 	opacityText: { previous: 0, current: 0 },
+// 		// 	angle: { previous: 0, current: 0 },
+// 		// 	squeeze: { previous: 0, current: 0 },
+// 		// 	force: 30,
+// 		};
+// 	}
 
-	preInit() {
-		this.getPosition();
+// 	preInit() {
+// 		this.getPosition();
 
-		properties.onHover.add((hoverState) => {
-			if (hoverState === HOVER_STATE.HI) {
-				this.enter(3, 1, 'hi', 5);
-			} else if (hoverState === HOVER_STATE.OPEN) {
-				this.enter(4, 1, 'open', 5);
-			} else if (hoverState === HOVER_STATE.DEFAULT) {
-				this.leave();
-			}
-		});
-	}
+// 		properties.onHover.add((hoverState) => {
+// 			if (hoverState === HOVER_STATE.HI) {
+// 				this.enter(3, 1, 'hi', 5);
+// 			} else if (hoverState === HOVER_STATE.OPEN) {
+// 				this.enter(4, 1, 'open', 5);
+// 			} else if (hoverState === HOVER_STATE.HIDE) {
+// 				this.hide();
+// 			} else if (hoverState === HOVER_STATE.DEFAULT) {
+// 				this.show();
+// 			}
+// 		});
+// 	}
 
-	init() {}
+// 	init() {}
 
-	show() {
-		this.cursor.style.opacity = 1;
-		this.active = true;
-		this.state.force = 30;
-	}
+// 	show() {
+// 		this.cursor.style.display = 'flex';
+// 		this.active = true;
+// 		this.state.scale.current = 1;
+// 		this.state.opacityCursor.current = 0.4;
+// 		this.state.opacityText.current = 0;
+// 		this.state.force = 20;
+// 	}
 
-	hide() {
-		this.cursor.style.opacity = 0;
-		this.active = false;
-	}
+// 	hide() {
+// 		this.cursor.style.display = 'none';
+// 		this.active = false;
+// 	}
 
-	enter(scale, opacity, textContent, force) {
-		// console.log('mouseenter');
-		this.state.scale.current = scale;
-		this.state.opacityCursor.current = opacity;
-		this.state.opacityText.current = opacity;
-		this.cursorContentText.textContent = textContent;
-		this.state.force = force;
-	}
+// 	enter(scale, opacity, textContent, force) {
+// 		// console.log('mouseenter');
+// 		this.state.scale.current = scale;
+// 		this.state.opacityCursor.current = opacity;
+// 		this.state.opacityText.current = opacity;
+// 		this.cursorContentText.textContent = textContent;
+// 		this.state.force = force;
+// 	}
 
-	leave() {
-		// console.log('mouseleave');
-		this.state.scale.current = 1;
-		this.state.opacityCursor.current = 0.4;
-		this.state.opacityText.current = 0;
-		this.state.force = 20;
-	}
+// 	getPosition() {
+// 		this.state.position.cursor = this.cursor.getBoundingClientRect();
+// 	}
 
-	getPosition() {
-		this.state.position.cursor = this.cursor.getBoundingClientRect();
-	}
+// 	getSqueeze(diffX, diffY) {
+// 		const distance = math.distanceTo(diffX, diffY);
+// 		const maxSqueeze = 0.1;
+// 		const accelerator = 1500;
+// 		return Math.min(distance / accelerator, maxSqueeze) * this.state.force;
+// 	}
 
-	getSqueeze(diffX, diffY) {
-		const distance = math.distanceTo(diffX, diffY);
-		const maxSqueeze = 0.1;
-		const accelerator = 1500;
-		return Math.min(distance / accelerator, maxSqueeze) * this.state.force;
-	}
+// 	resize(width, height) {}
 
-	resize(width, height) {}
+// 	delete() {}
 
-	delete() {}
+// 	update(dt) {
+// 		if (!this.active) return;
 
-	update(dt) {
-		if (!this.active) return;
+// 		let { width, height } = this.state.position.cursor;
+// 		this.state.x.current = input.mousePixelXY.x - width / 2;
+// 		this.state.y.current = input.mousePixelXY.y - height / 2;
 
-		let { width, height } = this.state.position.cursor;
-		this.state.x.current = input.mousePixelXY.x - width / 2;
-		this.state.y.current = input.mousePixelXY.y - height / 2;
+// 		// lerp old and current values
+// 		for (const el in this.state) {
+// 			if (Math.abs(this.state[el].previous - this.state[el].current) > 0.001) {
+// 				this.allowAnimation = true;
+// 				this.state[el].previous = math.lerp(this.state[el].previous, this.state[el].current, 0.25);
+// 			}
+// 		}
 
-		// lerp old and current values
-		for (const el in this.state) {
-			if (Math.abs(this.state[el].previous - this.state[el].current) > 0.001) {
-				this.allowAnimation = true;
-				this.state[el].previous = math.lerp(this.state[el].previous, this.state[el].current, 0.25);
-			}
-		}
+// 		// calculate difference in lerped values
+// 		const diffX = Math.round(this.state.x.current - this.state.x.previous);
+// 		const diffY = Math.round(this.state.y.current - this.state.y.previous);
 
-		// calculate difference in lerped values
-		const diffX = Math.round(this.state.x.current - this.state.x.previous);
-		const diffY = Math.round(this.state.y.current - this.state.y.previous);
+// 		this.state.squeeze.current = this.getSqueeze(diffX, diffY);
+// 		this.state.angle.current = math.getAngle(diffX, diffY);
 
-		this.state.squeeze.current = this.getSqueeze(diffX, diffY);
-		this.state.angle.current = math.getAngle(diffX, diffY);
+// 		const scale = `scale(${this.state.scale.previous})`;
+// 		const squeeze = `scale(${1 + this.state.squeeze.current}, ${1 - this.state.squeeze.current * 0.25})`;
+// 		const rotate = `rotate(${this.state.angle.current}deg)`;
+// 		const translate = `translateX(${this.state.x.previous}px) translateY(${this.state.y.previous}px) `;
 
-		const scale = `scale(${this.state.scale.previous})`;
-		const squeeze = `scale(${1 + this.state.squeeze.current}, ${1 - this.state.squeeze.current * 0.25})`;
-		const rotate = `rotate(${this.state.angle.current}deg)`;
-		const translate = `translateX(${this.state.x.previous}px) translateY(${this.state.y.previous}px) `;
+// 		if (this.allowAnimation) {
+// 			this.cursor.style.transform = translate;
+// 			this.cursor.style.opacity = this.state.opacityCursor.previous;
+// 			this.cursorSphere.style.transform = scale + rotate + squeeze;
+// 			this.cursorContent.style.opacity = this.state.opacityText.previous;
+// 		}
+// 	}
+// }
 
-		if (this.allowAnimation) {
-			this.cursor.style.transform = translate;
-			this.cursor.style.opacity = this.state.opacityCursor.previous;
-			this.cursorSphere.style.transform = scale + rotate + squeeze;
-			this.cursorContent.style.opacity = this.state.opacityText.previous;
-		}
-	}
-}
-
-export default new Cursor();
+// export default new Cursor();
